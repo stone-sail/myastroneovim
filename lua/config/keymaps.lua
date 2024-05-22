6d8598c87e04c19ea170c311203364a90a65a7c5
stone <stone>
2024-05-22 14:57:37 +0800

    feat: add ctrl+v ctrl+c

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,4 +12,10 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "<localleader>ec", ":cd ~/.config/nvim<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-V>", "<C-V>", { noremap = true })
+
+-- 复制到系统剪贴板
+vim.api.nvim_set_keymap("n", "<C-c>", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
+-- 从系统剪贴板粘贴
+vim.api.nvim_set_keymap("n", "<C-v>", '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
